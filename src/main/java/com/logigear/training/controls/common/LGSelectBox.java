deab1c51f063c40bffba6d23e9040896a704c468
daodao <daodepgai@gmail.com>
2020-06-22 23:28:25 +0700

    add tesst case 012

---
+++ src/main/java/com/logigear/training/controls/common/LGSelectBox.java
@@ -0,0 +1,10 @@
+package com.logigear.training.controls.common;
+
+import com.logigear.training.controls.base.Clickable;
+
+public class LGSelectBox extends Clickable {
+    public LGSelectBox (String locator) {
+        super();
+        this.locator = locator;
+    }
+    }
--- src/test/java/com/logigear/training/test/DA_LOGIN/DA_LOGIN_TC0010_VerifyThatThePageWorksCorrectlyForTheCaseWhenNoInputEnteredToPasswordAndUsernamefield.java
+++ src/test/java/com/logigear/training/test/DA_LOGIN/DA_LOGIN_TC0010_VerifyThatThePageWorksCorrectlyForTheCaseWhenNoInputEnteredToPasswordAndUsernamefield.java
@@ -1,7 +1,7 @@
 package com.logigear.training.test.DA_LOGIN;
 
 import com.logigear.training.common.Constant;
-import com.logigear.training.pages.GeneralPage;
+import com.logigear.training.pages.DashBoardPage;
 import com.logigear.training.pages.LoginPage;
 import com.logigear.training.test.BaseTest;
 import org.testng.Assert;
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 public class DA_LOGIN_TC0010_VerifyThatThePageWorksCorrectlyForTheCaseWhenNoInputEnteredToPasswordAndUsernamefield extends BaseTest {
 	LoginPage loginPage = new LoginPage();
-	GeneralPage generalPage = new GeneralPage();
+	DashBoardPage dashBoardPage = new DashBoardPage();
 
 	@Test
 	public void TC0010() {
