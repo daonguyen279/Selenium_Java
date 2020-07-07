de61d12c01de68f3d5091d575758d237f2984ae4
daodao <daodepgai@gmail.com>
2020-07-07 23:15:24 +0700

    update action addpage

--- src/test/java/com/logigear/training/test/DA_MP/DA_MP_TC0012_VerifyThatUserIsAbleToAddAdditionalPagesBesidesOverviewPageSuccessfully.java
+++ src/test/java/com/logigear/training/test/DA_MP/DA_MP_TC0012_VerifyThatUserIsAbleToAddAdditionalPagesBesidesOverviewPageSuccessfully.java
@@ -10,22 +10,14 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
-public class DA_MP extends BaseTest {
-    @DataProvider(name = "data-provider")
-    public Object[][] dataProviderMethod() {
-        return new Object[][]{
-                {"page 1", null, null, null, ""},
-                {"page 2", "page 1", null, null, ""}
-        };
-    }
+public class DA_MP_TC0012_VerifyThatUserIsAbleToAddAdditionalPagesBesidesOverviewPageSuccessfully extends BaseTest {
 
-    @Test(dataProvider = "data-provider")
-    public void TC(String pageName, String parentPage, String columnNumber, String position, String expect) {
+    public void TC0012(String pageName, String parentPage, String columnNumber, String position, String expect) {
         LoginPage loginPage = new LoginPage();
         loginPage.loginWith(Constant.UPERCASE_USERNAME, Constant.PASSWORD);
         DashBoardPage dashBoardPage = new DashBoardPage();
         dashBoardPage.clickAddPage();
-        dashBoardPage.inputPageInformation(pageName, parentPage, columnNumber, position);
+        dashBoardPage.inputPageInformation(pageName, null, null, null);
         dashBoardPage.submitPageInformation();
     }
 }
